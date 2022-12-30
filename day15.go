72df32b9d30d8989daea20003516adc4ca47f5d0
iBug <git@ibugone.com>
2022-12-31 03:24:48 +0800

    Update 15-2 (very fast)

--- day15.go
+++ day15.go
@@ -81,6 +81,25 @@ func Solution15_1(r io.Reader) {
 	fmt.Println(total)
 }
 
+type Line15 struct {
+	// x + by = c, delimited by x1 and x2
+	// assume x1 < x2 and Abs(b) == 1
+	x1, x2, b, c int
+}
+
+func LineIntersect15(l1, l2 Line15) (Coord, bool) {
+	det := l2.b - l1.b
+	if det == 0 || (l2.c-l1.c)%2 == 1 {
+		return Coord{0, 0}, false
+	}
+	x := (l2.b*l1.c - l1.b*l2.c) / det
+	if x < l1.x1 || x < l2.x1 || x >= l1.x2 || x >= l2.x2 {
+		return Coord{0, 0}, false
+	}
+	y := (l2.c - l1.c) / det
+	return Coord{x, y}, true
+}
+
 func Solution15_2(r io.Reader) {
 	type Sensor struct {
 		Coord
@@ -97,30 +116,25 @@ func Solution15_2(r io.Reader) {
 		sensors = append(sensors, Sensor{Coord{sx, sy}, r})
 	}
 
-	edges := make(map[string]int)
+	lines := make([]Line15, 0, 200)
+	intersects := make(map[string]int)
 	for _, s := range sensors {
-		c1 := Coord{s.x - s.r - 1, s.y}
-		c2 := Coord{s.x + s.r + 1, s.y}
-		c3 := Coord{s.x, s.y - s.r - 1}
-		c4 := Coord{s.x, s.y + s.r + 1}
-		for i := 0; i <= s.r; i++ {
-			edges[c1.String()]++
-			edges[c2.String()]++
-			edges[c3.String()]++
-			edges[c4.String()]++
-			c1.x++
-			c1.y++
-			c2.x--
-			c2.y--
-			c3.x--
-			c3.y++
-			c4.x++
-			c4.y--
+		lines = append(lines, Line15{s.x - s.r - 1, s.x + 1, 1, s.x + s.y - s.r - 1})
+		lines = append(lines, Line15{s.x - s.r - 1, s.x + 1, -1, s.x - s.y - s.r - 1})
+		lines = append(lines, Line15{s.x, s.x + s.r + 2, 1, s.x + s.y + s.r + 1})
+		lines = append(lines, Line15{s.x, s.x + s.r + 2, -1, s.x - s.y + s.r + 1})
+	}
+	for i := 0; i < len(lines); i++ {
+		for j := i + 1; j < len(lines); j++ {
+			if p, ok := LineIntersect15(lines[i], lines[j]); ok {
+				intersects[p.String()]++
+			}
 		}
 	}
+
 	var x, y int
 	maxK, maxV := "", 0
-	for k, v := range edges {
+	for k, v := range intersects {
 		fmt.Sscanf(k, "%d,%d", &x, &y)
 		for _, s := range sensors {
 			if Abs(x-s.x)+Abs(y-s.y) <= s.r {
@@ -134,5 +148,5 @@ func Solution15_2(r io.Reader) {
 		}
 	}
 	fmt.Sscanf(maxK, "%d,%d", &x, &y)
-	fmt.Println(4000000*int64(x) + int64(y))
+	fmt.Println(maxK, 4000000*int64(x)+int64(y))
 }
