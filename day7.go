41975d0c7a9ab2f1be0dbb0c4f75ce94840c8462
iBug <git@ibugone.com>
2022-12-30 19:26:54 +0800

    Add 7-2

--- day7.go
+++ day7.go
@@ -4,13 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math"
 	"strconv"
 	"strings"
 )
 
 func init() {
-	RegisterSolution("7-1", Solution7_1)
-	// RegisterSolution("7-2", Solution7_2)
+	RegisterSolution("7", Solution7)
+	RegisterSolution("7-1", Solution7)
+	RegisterSolution("7-2", Solution7)
 }
 
 type Inode struct {
@@ -57,7 +59,25 @@ func RecurseTotalSize_7(dir *Inode, thresh int) int {
 	return total
 }
 
-func Solution7_1(r io.Reader) {
+func RecurseMinimumSize_7(dir *Inode, thresh int) int {
+	total := math.MaxInt
+	if dir.size >= thresh && dir.size < total {
+		total = dir.size
+	}
+
+	for _, inode := range dir.children {
+		if inode.mode == M_FILE {
+			continue
+		}
+		size := RecurseMinimumSize_7(inode, thresh)
+		if size >= thresh && size < total {
+			total = size
+		}
+	}
+	return total
+}
+
+func Solution7(r io.Reader) {
 	root := Inode{mode: M_DIR, children: make(map[string]*Inode, 0)}
 	cwd := &root
 	root.parent = &root
@@ -86,4 +106,9 @@ func Solution7_1(r io.Reader) {
 
 	thresh := 100000
 	fmt.Println(RecurseTotalSize_7(&root, thresh))
+
+	fs_total := 70000000
+	fs_required := 30000000
+	fs_needs := fs_required - (fs_total - DirSizeRecurse_7(&root))
+	fmt.Println(RecurseMinimumSize_7(&root, fs_needs))
 }
