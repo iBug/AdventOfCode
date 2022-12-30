b5cab117a975d4da3a8a4192058baf7d2710fab4
iBug <git@ibugone.com>
2022-12-30 17:12:26 +0800

    Add 3-2

--- day3.go
+++ day3.go
@@ -8,7 +8,15 @@ import (
 
 func init() {
 	RegisterSolution("3-1", Solution3_1)
-	// RegisterSolution("3-2", Solution3_2)
+	RegisterSolution("3-2", Solution3_2)
+}
+
+func Day3_priority(c byte) int {
+	if c <= 'Z' {
+		return int(26 + c - 'A')
+	} else {
+		return int(c - 'a')
+	}
 }
 
 func Solution3_1(r io.Reader) {
@@ -20,23 +28,37 @@ outer:
 		b := make([]bool, 52)
 		mid := len(s) / 2
 		for i := 0; i < mid; i++ {
-			if s[i] <= 'Z' {
-				b[26+s[i]-'A'] = true
-			} else {
-				b[s[i]-'a'] = true
-			}
+			b[Day3_priority(s[i])] = true
 		}
 		for i := mid; i < len(s); i++ {
-			if s[i] <= 'Z' {
-				if b[26+s[i]-'A'] {
-					total += int(27 + s[i] - 'A')
-					continue outer
-				}
-			} else {
-				if b[s[i]-'a'] {
-					total += int(1 + s[i] - 'a')
-					continue outer
-				}
+			prio := Day3_priority(s[i])
+			if b[prio] {
+				total += 1 + prio
+				continue outer
+			}
+		}
+	}
+	fmt.Println(total)
+}
+
+func Solution3_2(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	total := 0
+outer:
+	for scanner.Scan() {
+		b := make([]byte, 52)
+		for _, c := range scanner.Text() {
+			b[Day3_priority(byte(c))] = 1
+		}
+		scanner.Scan()
+		for _, c := range scanner.Text() {
+			b[Day3_priority(byte(c))] |= 2
+		}
+		scanner.Scan()
+		for _, c := range scanner.Text() {
+			if b[Day3_priority(byte(c))] == 3 {
+				total += 1 + Day3_priority(byte(c))
+				continue outer
 			}
 		}
 	}
