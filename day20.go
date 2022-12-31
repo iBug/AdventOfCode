57492cfc9cf0095a62c95843d0b8bc19bb906d77
iBug <git@ibugone.com>
2023-01-01 03:25:36 +0800

    Add 20-2

--- day20.go
+++ day20.go
@@ -8,47 +8,21 @@ import (
 )
 
 func init() {
-	RegisterSolution("20-1", Solution20_1)
-	// RegisterSolution("20-2", Solution20_2)
+	RegisterSolution("20-1", func(r io.Reader) { Solution20(r, 1) })
+	RegisterSolution("20-2", func(r io.Reader) { Solution20(r, 2) })
 }
 
 type Node20 struct {
-	v          int
+	v          int64
 	prev, next *Node20
 }
 
-func PrintList20(n *Node20, count int) {
-	p := n
-	fmt.Printf("[%d", p.v)
-	for i := 1; i < count; i++ {
-		p = p.next
-		fmt.Printf(", %d", p.v)
-	}
-	fmt.Println("]")
-}
+const MAGIC_KEY_20 = 811589153
 
-func Solution20_1(r io.Reader) {
-	scanner := bufio.NewScanner(r)
-	n := make([]*Node20, 0, 5000)
-	zeroPos := 0
-	for scanner.Scan() {
-		v, _ := strconv.Atoi(scanner.Text())
-		n = append(n, &Node20{v: v})
-		if v == 0 {
-			zeroPos = len(n) - 1
-		}
-	}
-
-	n[0].prev = n[len(n)-1]
-	n[len(n)-1].next = n[0]
-	for i := 0; i < len(n)-1; i++ {
-		n[i].next = n[i+1]
-		n[i+1].prev = n[i]
-	}
-
+func MixList20(n []*Node20) {
 	for i := 0; i < len(n); i++ {
 		p := n[i]
-		v := p.v
+		v := p.v % int64(len(n)-1)
 		if v == 0 {
 			continue
 		}
@@ -78,8 +52,49 @@ func Solution20_1(r io.Reader) {
 			t.next = p
 		}
 	}
+}
 
-	total := 0
+func PrintList20(n *Node20, count int) {
+	p := n
+	fmt.Printf("[%d", p.v)
+	for i := 1; i < count; i++ {
+		p = p.next
+		fmt.Printf(", %d", p.v)
+	}
+	fmt.Println("]")
+}
+
+func Solution20(r io.Reader, mode int) {
+	scanner := bufio.NewScanner(r)
+	n := make([]*Node20, 0, 5000)
+	zeroPos := 0
+	for scanner.Scan() {
+		v, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+		if mode == 2 {
+			v *= MAGIC_KEY_20
+		}
+		n = append(n, &Node20{v: v})
+		if v == 0 {
+			zeroPos = len(n) - 1
+		}
+	}
+
+	n[0].prev = n[len(n)-1]
+	n[len(n)-1].next = n[0]
+	for i := 0; i < len(n)-1; i++ {
+		n[i].next = n[i+1]
+		n[i+1].prev = n[i]
+	}
+
+	mixTimes := 1
+	if mode == 2 {
+		mixTimes = 10
+	}
+	for i := 0; i < mixTimes; i++ {
+		MixList20(n)
+	}
+
+	total := int64(0)
 	p := n[zeroPos]
 	for i := 0; i < 3000; i++ {
 		p = p.next
