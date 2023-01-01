d2a7b17490f7d910285913db80f051d40eb4d3d3
iBug <git@ibugone.com>
2023-01-02 02:48:57 +0800

    Auto search for input file

--- README.md
+++ README.md
@@ -12,21 +12,6 @@ My solutions for [Advent Of Code 2022](https://adventofcode.com/2022/) challenge
 
 ## Usage
 
-Compilation is straightforward: `make` (or just `go build`)
+Compilation is straightforward: `make` (or `go build`)
 
-Built-in program help:
-
-```text
-Usage: ./adventofcode [option...] <solution> [input...]
-
-Available options:
-  -p    show performance information
-
-Available solutions:
-  1-1, 1-2, 10-1, 10-2, 11-1, 11-2, 12-1, 12-2, 13-1, 13-2, 14-1, 14-2, 15-1,
-  15-2, 16-1, 16-2, 17-1, 17-2, 18-1, 18-2, 19-1, 19-2, 2-1, 2-2, 20-1, 20-2,
-  21-1, 21-2, 22-1, 22-2, 22-2e, 23-1, 23-2, 24, 24-1, 24-2, 25, 25-1, 3-1, 3-2,
-  4-1, 4-2, 5-1, 5-2, 6-1, 6-2, 7, 7-1, 7-2, 8-1, 8-2, 9-1, 9-1a, 9-2
-```
-
-If no input file is specified, it uses standard input.
+The program has built-in help message available with `-h` flag.
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path"
 	"reflect"
 	"runtime"
 	"sort"
@@ -44,7 +45,10 @@ func Usage() {
 	sort.Strings(solutions)
 
 	w := flag.CommandLine.Output()
-	fmt.Fprintf(w, "Usage: %s [option...] <solution> [input...]\n", os.Args[0])
+	fmt.Fprintf(w, "Usage: %s [option...] <solution> [input]\n\n", os.Args[0])
+	fmt.Fprint(w, "Run the specified solution.\n"+
+		"If no input file is given, attempts to search for an appropriate one.\n"+
+		"If input file is -, reads from stdin.\n")
 	fmt.Fprintf(w, "\nAvailable options:\n")
 	flag.VisitAll(func(f *flag.Flag) {
 		fmt.Fprintf(w, "  -%s\t%s\n", f.Name, f.Usage) // f.Name, f.Value
@@ -67,45 +71,91 @@ func Usage() {
 	fmt.Fprintln(w)
 }
 
-var fShowPerformance bool
+func IsTerminal(f *os.File) bool {
+	fi, err := f.Stat()
+	if err != nil {
+		return false
+	}
+	return fi.Mode()&os.ModeCharDevice != 0
+}
+
+func FindInputFile(name string) string {
+	name = strings.SplitN(name, "-", 2)[0]
+	candidates := make([]string, 0, 12)
+	for _, dir := range []string{"inputs", "input", ""} {
+		for _, ext := range []string{".txt", ".in"} {
+			candidates = append(candidates, path.Join(dir, "input-"+name+ext))
+			candidates = append(candidates, path.Join(dir, name+ext))
+		}
+		candidates = append(candidates, path.Join(dir, "day"+name, "input.txt"))
+		candidates = append(candidates, path.Join(dir, "input-"+name))
+	}
+
+	for _, c := range candidates {
+		if _, err := os.Stat(c); err == nil {
+			return c
+		}
+	}
+	return ""
+}
 
 func main() {
+	var fShowPerformance bool
 	flag.Usage = Usage
 	flag.BoolVar(&fShowPerformance, "p", false, "show performance information")
 	flag.Parse()
 	if flag.NArg() < 1 {
-		Usage()
+		flag.Usage()
 		os.Exit(1)
 	}
 
 	solution := NormalizeName(flag.Arg(0))
 	fn, ok := registry[solution]
 	if !ok {
-		Usage()
-		fmt.Fprintf(os.Stderr, "Unknown solution: %s\n", solution)
+		flag.Usage()
+		fmt.Fprintf(os.Stderr, "\nUnknown solution: %s\n", solution)
 		os.Exit(1)
 	}
-	rs := []io.Reader{}
-	for _, path := range flag.Args()[1:] {
+
+	var r io.Reader
+	if flag.NArg() == 1 {
+		path := FindInputFile(solution)
+		if path == "" {
+			fmt.Fprintf(os.Stderr, "Cannot find input file for %s\n", solution)
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stderr, "Found input file: %s\n", path)
 		f, err := os.Open(path)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Cannot open %s: %v\n", path, err)
 			os.Exit(1)
 		}
 		defer f.Close()
-		rs = append(rs, f)
-	}
-	if len(rs) == 0 {
-		rs = append(rs, os.Stdin)
+		r = f
+	} else {
+		if flag.Arg(1) == "-" {
+			r = os.Stdin
+		} else {
+			path := flag.Arg(1)
+			f, err := os.Open(path)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Cannot open %s: %v\n", path, err)
+				os.Exit(1)
+			}
+			defer f.Close()
+			r = f
+		}
 	}
 
 	pState := GetPerformanceState()
-	fn(io.MultiReader(rs...))
+	fn(r)
 	pInfo := DiffPerformanceState(pState)
 
 	if fShowPerformance {
 		w := os.Stderr
-		fmt.Fprintln(w)
+		if IsTerminal(os.Stdout) && IsTerminal(os.Stderr) {
+			fmt.Fprintln(w)
+		}
 		PrintPerformanceInfo(w, pInfo)
 	}
 }
